08fe757dadda07f60ff1a61e905ea17031785999
elvish-ishaan <ishaanelvish@gmail.com>
2025-02-03 00:13:21 +0530

    init

--- app/api/payments/payment-varification/route.ts
+++ app/api/payments/payment-varification/route.ts
@@ -1,59 +1,59 @@
-import { razorpayInstance } from '@/app/configs/paymentConfig'
-import { NextRequest, NextResponse } from 'next/server'
-import crypto from 'crypto'
-import { NextApiRequest } from 'next'
+// import { razorpayInstance } from '@/app/configs/paymentConfig'
+// import { NextRequest, NextResponse } from 'next/server'
+// import crypto from 'crypto'
+// import { NextApiRequest } from 'next'
 
-export const POST = async (req: NextApiRequest) => {
-    console.log(req.body, 'getting body for vari')
-    try {
-        //getting payment varification
-        const { razorpay_payment_id, razorpay_order_id, razorpay_signature } =
-            req.body
+// export const POST = async (req: NextApiRequest) => {
+//     console.log(req.body, 'getting body for vari')
+//     try {
+//         //getting payment varification
+//         const { razorpay_payment_id, razorpay_order_id, razorpay_signature } =
+//             req.body
 
-        try {
-            if (
-                !razorpay_payment_id ||
-                !razorpay_order_id ||
-                !razorpay_signature
-            ) {
-                return NextResponse.json({
-                    success: false,
-                    message: 'required parameters not found',
-                })
-            }
+//         try {
+//             if (
+//                 !razorpay_payment_id ||
+//                 !razorpay_order_id ||
+//                 !razorpay_signature
+//             ) {
+//                 return NextResponse.json({
+//                     success: false,
+//                     message: 'required parameters not found',
+//                 })
+//             }
 
-            // varifying signiture
-            const secret: string | undefined = process.env.RAZORPAY_KEY_SECRET
-            const body_data = razorpay_order_id + '|' + razorpay_payment_id
-            const expectedSignature = crypto
-                .createHmac('sha256', secret || 'defaultSecret')
-                .update(body_data)
-                .digest('hex')
-            // comparing signitures
-            const isValid = expectedSignature === razorpay_signature
-            // Retrieve the order details from Razorpay
-            const order = await razorpayInstance.orders.fetch(razorpay_order_id)
+//             // varifying signiture
+//             const secret: string | undefined = process.env.RAZORPAY_KEY_SECRET
+//             const body_data = razorpay_order_id + '|' + razorpay_payment_id
+//             const expectedSignature = crypto
+//                 .createHmac('sha256', secret || 'defaultSecret')
+//                 .update(body_data)
+//                 .digest('hex')
+//             // comparing signitures
+//             const isValid = expectedSignature === razorpay_signature
+//             // Retrieve the order details from Razorpay
+//             const order = await razorpayInstance.orders.fetch(razorpay_order_id)
 
-            //if varified
-            if (isValid) {
-                console.log('order varified..........')
-                //update the booking data
-                //redirect to success page
-                // return res.redirect('http://localhost:5173/payment/success')
-            } else {
-                console.log('order not varified.......')
-                // return res.redirect('http://localhost:5173/payment/failed')
-            }
-        } catch (error) {
-            console.log(error, 'error in payment varification')
-        }
-    } catch (error) {
-        return NextResponse.json(
-            {
-                success: false,
-                message: 'Internal Server Error',
-            },
-            { status: 500 }
-        )
-    }
-}
+//             //if varified
+//             if (isValid) {
+//                 console.log('order varified..........')
+//                 //update the booking data
+//                 //redirect to success page
+//                 // return res.redirect('http://localhost:5173/payment/success')
+//             } else {
+//                 console.log('order not varified.......')
+//                 // return res.redirect('http://localhost:5173/payment/failed')
+//             }
+//         } catch (error) {
+//             console.log(error, 'error in payment varification')
+//         }
+//     } catch (error) {
+//         return NextResponse.json(
+//             {
+//                 success: false,
+//                 message: 'Internal Server Error',
+//             },
+//             { status: 500 }
+//         )
+//     }
+// }
